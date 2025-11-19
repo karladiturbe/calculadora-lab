5b0919db711acd5025148ad182e34c0037481184
Karla Iturbe <karladiturbep@gmail.com>
2025-11-19 15:15:20 -0600

    feat: prompts

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return parseFloat(num1) / parseFloat(num2);
 }
-let result = add(2.56,"5");
+let num1 = prompt("Primer valor");
+let num2 = prompt("Segundo valor");
+let result = add(num1, num2);
 console.log(result);
-let result2 = substract(2.56,"5");
+let result2 = substract(num1, num2);
 console.log(result2);
-let result3 = divide(2.56,"5");
+let result3 = divide(num1, num2);
 console.log(result3);
-let result4 = multiply(2.56,"5");
+let result4 = multiply(num1, num2);
 console.log(result4);
